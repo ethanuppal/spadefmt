029b7adc747e4cc9d44f4c0a45af928e06d7314e
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-02-20 16:33:18 -0500

    backup

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::{
     sync::RwLock,
 };
 
-use snafu::{whatever, ResultExt, Whatever};
+use snafu::{whatever, OptionExt, ResultExt, Whatever};
 pub use spade;
 use spade_codespan_reporting::{files::SimpleFiles, term::termcolor::Buffer};
 use spade_diagnostics::{emitter::CodespanEmitter, CodeBundle, DiagHandler};
@@ -32,6 +32,7 @@ use spadefmt::{
     document_builder::DocumentBuilder,
     resolve_try_catch::{resolve_try_catch, PrintingContext},
 };
+use type_sitter::{Node, Parser as TSParser};
 
 #[snafu::report]
 fn main() -> Result<(), Whatever> {
@@ -79,26 +80,38 @@ fn main() -> Result<(), Whatever> {
         FILE_ID,
     );
 
-    let root = match parser.top_level_module_body() {
-        Ok(root) => root,
-        Err(error) => {
-            error_handler.report(&error);
-            for error in &parser.errors {
-                error_handler.report(error);
-            }
-            whatever!("Exiting due to errors")
+    if let Err(error) = parser.top_level_module_body() {
+        error_handler.report(&error);
+        for error in &parser.errors {
+            error_handler.report(error);
         }
-    };
+        whatever!("Exiting due to errors");
+    }
 
-    let test_contents = fs::read_to_string("test.toml")
-        .whatever_context("test file test.toml should be there")?;
+    let mut tree_sitter_parser = TSParser::new(
+        &tree_sitter_spade::language().into(),
+    )
+    .whatever_context("Failed to set tree-sitter parser language to Spade")?;
+    let root = tree_sitter_parser
+        .parse(code, None)
+        .expect("Internal Spade bug: tree-sitter could not parse code accepted by the Spade parser.
+Please file a bug report at <https://gitlab.com/spade-lang/tree-sitter-spade>.");
+    let type_sitter_tree = type_sitter_spade::SourceFile::try_from_raw(
+        root.root_node(),
+    )
+        .expect("Internal Spade bug: tree-sitter parser parsed top level as a node that isn't SourceFile");
+
+    let test_contents = fs::read_to_string(opts.config).whatever_context(
+        format!("Failed to read config file at {}", opts.config),
+    )?;
     let test_config = toml::from_str::<Config>(&test_contents)
         .whatever_context("Failed to decode config")?;
 
     let indent = test_config.indent.inner;
+    let cursor = type_sitter_tree.walk();
     let (mut document_store, root_idx) =
         DocumentBuilder::new(test_config.indent.inner as isize)
-            .build_root(&root);
+            .build_root(&root, cursor);
 
     if opts.debug {
         let mut buffer = String::new();
