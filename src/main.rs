c997ca230653e2567fdb3862060cddfbd702941f
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-09-10 05:13:18 -0400

    chore: rustfmt
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,17 @@ use std::{
     sync::RwLock,
 };
 
-use snafu::{whatever, ResultExt, Whatever};
+use snafu::{ResultExt, Whatever, whatever};
 pub use spade;
 use spade_codespan_reporting::{files::SimpleFiles, term::termcolor::Buffer};
-use spade_diagnostics::{emitter::CodespanEmitter, CodeBundle, DiagHandler};
+use spade_diagnostics::{CodeBundle, DiagHandler, emitter::CodespanEmitter};
 use spade_parser::logos::Logos;
 use spadefmt::{
     cli::Opts,
     config::Config,
     document,
     document_builder::DocumentBuilder,
-    resolve_try_catch::{resolve_try_catch, PrintingContext},
+    resolve_try_catch::{PrintingContext, resolve_try_catch},
 };
 
 #[snafu::report]
